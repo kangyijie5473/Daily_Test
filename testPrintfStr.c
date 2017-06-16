dd1f9b9bcc012e2a964a102753d9b2659c4beda2
kang <xiyoulinux.kangyijie@gmail.com>
2017-06-16 08:07:39 +0800

    printf(str)用法

---
+++ testPrintfStr.c
@@ -0,0 +1,15 @@
+/*
+ * > File Name: testPrintfStr.c
+ * > Author: Jack Kang
+ * > Mail: kangyijie@xiyoulinux.org
+ * > Created Time: 2017年06月16日 星期五 08时01分35秒
+ */
+
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+    char *sstr = "hello";
+    printf(ssstr); //printf 也可以并不加格式化参数，直接打印字符串
+                   //但是和puts不同没有默认换行
+}
---
+++ testprintf.c
@@ -0,0 +1,12 @@
+/*
+ * > File Name: testprintf.c
+ * > Author: Jack Kang
+ * > Mail: kangyijie@xiyoulinux.org
+ * > Created Time: 2017年04月14日 星期五 21时05分37秒
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("hello" "wr");
+}
