4a8c012134ab4be7f8b4bdb56f822b4c1550cbd9
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-07-26 21:46:56 +0800

    std::move之后析构

--- testMoveList.cpp
+++ testMoveList.cpp
@@ -7,6 +7,13 @@ int main(void)
     string a("str");
     vector<string> b;
     b.push_back(std::move(a));
-    cout << a << endl; // 似乎move完a为空了
+    cout << a << endl; // 似乎move完a析构了
+    int aa = 3;
+    vector<int> bb;
+    bb.push_back(std::move(aa));
+    cout << aa << endl; //aa没有析构函数 值不变
+    aa++;
+    for(auto i : bb)
+        cout << i << endl;//对于基本的aa直接拷贝
     
 }
