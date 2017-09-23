f5ad362a7a1ac433ba24f76815a5ccc5e2fa3774
kangkang <xiyoulinux.kangyijie@gmail.com>
2017-09-23 11:27:48 +0800

    redis macro UNUSED

---
+++ testUNUSED.c
@@ -0,0 +1,14 @@
+/*
+ * > File Name: testUNUSED.c
+ * > Author: Jack Kang
+ * > Mail: kangyijie@xiyoulinux.org
+ * > Created Time: 2017年09月23日 星期六 10时20分41秒
+ */
+
+#include <stdio.h>
+#define UNUSED(v) ((void)v)
+int main(int argc, char **argv)
+{
+    //UNUSED(argc);
+    argc = 2.00;
+}
